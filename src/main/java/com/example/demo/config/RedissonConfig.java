042c30f892104922ee82f74d3f21510621295d9f
yujiyeon26 <135409568+yujiyeon26@users.noreply.github.com>
2023-06-04 12:03:10 +0900

    Update RedissonConfig.java

--- src/main/java/com/example/demo/config/RedissonConfig.java
+++ src/main/java/com/example/demo/config/RedissonConfig.java
@@ -41,23 +41,4 @@ public class RedissonConfig {
         return redisson;
     }
    
-    /* Cluster 구성 했을때.
-    @Profile({"prod","dev"})
-    @Bean(name = "redissonClient")
-    public RedissonClient redissonClientCluster() throws IOException {
-        String[] nodes = redisProperties.getUrl().split(",");   
-        for (int i = 0; i < nodes.length; i++) {
-            nodes[i] = "redis://" + nodes[i];
-        }
-        RedissonClient redisson = null;
-        Config config = new Config();
-        final Codec codec = new StringCodec(); // redis-cli에서 보기 위해
-        config.setCodec(codec);
-        config.useClusterServers()
-                .setScanInterval(2000)     
-                .addNodeAddress(nodes);
-        redisson = Redisson.create(config);   
-        return redisson;
-    }
-    */
 }
